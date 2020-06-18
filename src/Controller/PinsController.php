f31b3b715d06dae21cb7b1b055a56267a69a0155
gaston223 <g.coulibalyy@gmail.com>
2020-06-18 16:23:51 +0200

    Ajout du Bundle Slugify + propriété slug dans l'entité Pin

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,9 +15,21 @@ class PinsController extends AbstractController
      * @param PinRepository $pinRepository
      * @return Response
      */
-    public function index(PinRepository $pinRepository)
+    public function index(PinRepository $pinRepository):Response
     {
         $pins = $pinRepository->findAll();
         return $this->render('pins/index.html.twig', compact('pins'));
+    }
+
+
+    /**
+     * @Route("/pins/{slug}", name="app_pin_show")
+     * @param Pin $pin
+     * @return Response
+     */
+    public function show(Pin $pin):Response
+    {
+        dd($pin);
+        return $this->render('pins/show.html.twig', compact($pin));
     }
 }
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 
 use App\Repository\PinRepository;
+use Cocur\Slugify\Slugify;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
+ * @ORM\HasLifecycleCallbacks()
  */
 class Pin
 {
@@ -26,6 +28,27 @@ class Pin
      * @ORM\Column(type="text")
      */
     private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+
+    /**
+     * Permet d'initialiser le slug
+     *
+     *@ORM\PrePersist
+     *@ORM\PreUpdate
+     * @return void
+     */
+    public function initializeSlug()
+    {
+        if(empty($this->slug)){
+            $slugify= new Slugify();
+            $this->slug = $slugify->slugify($this->title);
+        }
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +75,18 @@ class Pin
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
