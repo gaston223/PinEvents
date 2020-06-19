d8535a29d312abf527aae1e3412c2af6e9ba39c3
gaston223 <g.coulibalyy@gmail.com>
2020-06-20 01:59:55 +0200

    Ajout des méthodes Créer et Modifier un Pin + Vues

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -4,26 +4,63 @@ namespace App\Controller;
 
 use App\Entity\Pin;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinsController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods={"GET"})
      * @param PinRepository $pinRepository
      * @return Response
      */
     public function index(PinRepository $pinRepository):Response
     {
-        $pins = $pinRepository->findBy([], ['createdAt' => 'ASC']);
+        $pins = $pinRepository->findBy([], ['createdAt' => 'DESC']);
         return $this->render('pins/index.html.twig', compact('pins'));
     }
 
 
     /**
-     * @Route("/pins/{slug}", name="app_pin_show")
+     * @Route("/pins/create", name="app_pins_create", methods={"GET|POST"})
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $pin = new Pin;
+        $form = $this->createFormBuilder($pin)
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+
+            $pin = $form->getData();
+            $em->persist($pin);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/pin/{slug}", name="app_pin_show", methods={"GET"})
      * @param Pin $pin
      * @return Response
      */
@@ -31,4 +68,39 @@ class PinsController extends AbstractController
     {
         return $this->render('pins/show.html.twig', compact('pin'));
     }
+
+
+    /**
+     * @Route("/pin/{slug}/edit", name="app_pin_edit", methods={"GET|POST"})
+     * @param Pin $pin
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function edit(Pin $pin, Request $request, EntityManagerInterface $em):Response
+    {
+        $form = $this->createFormBuilder($pin)
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+
+            $pin = $form->getData();
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/edit.html.twig', [
+            'form'=> $form->createView(),
+            'pin'=>$pin
+        ]);
+    }
+
+
 }
